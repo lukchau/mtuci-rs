8837f127a1abc582ed1b119044423e47358c2fd3
lukchau <lukagogokhiya1988@gmail.com>
2023-06-29 13:12:00 +0300

    after chapter 4

---
+++ ch4/src/references.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
---
+++ ch4/src/slices.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+
+    println!("{slice}")
+}
