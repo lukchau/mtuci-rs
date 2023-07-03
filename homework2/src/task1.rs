9cc9399888090b9efaa1155607c80996012cdf22
lukchau <lukagogokhiya1988@gmail.com>
2023-07-03 18:31:54 +0300

    homework 2

---
+++ homework2/src/task1.rs
@@ -0,0 +1,43 @@
+/*
+----> ЗАДАНИЕ 1 "Поиск слова в строке"
+
+Вывести номер строки в которой встречается нужное слово и саму строку в формате:
+номер строки: строка...
+
+ */
+
+const SEARCH_TERM: &str = "picture";
+const QUOTE: &str = "Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+
+
+fn main() {
+    find_term(SEARCH_TERM, QUOTE);
+}
+
+fn find_term(search_term: &str, quote: &str) -> String {
+    let lines: Vec<&str> = quote.lines().collect();
+
+    for (number, line) in lines.iter().enumerate() {
+        if line.contains(search_term) {
+            return format!("{}: {}", number + 1, line);
+        }
+    }
+    format!("Word was not found")
+}
+
+
+// ----> TESTS
+#[cfg(test)]
+mod tests {
+    use crate::find_term;
+    use crate::{SEARCH_TERM, QUOTE};
+
+    #[test]
+    fn correct_line() {
+        let answer = find_term(SEARCH_TERM, QUOTE);
+
+        assert_eq!("2: dark square is a picture feverishly turned--in search of what?", answer)
+    }
+}
